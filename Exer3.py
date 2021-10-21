93801b1e48a6e2e174ac0e4afa26982f213f600a
professoravanessamoncao <92949361+professoravanessamoncao@users.noreply.github.com>
2021-10-21 19:27:32 -0300

    Exer3

---
+++ Exer3.py
@@ -0,0 +1,12 @@
+#Exercício 3 - Ler 3 valores inteiros e imprimir a soma dos três
+print('Digite três valores inteiros!')
+print('Primeiro valor:')
+num1 = int(input())
+print('Segundo Valor:')
+num2 = int(input())
+print('Terceiro valor:')
+num3 = int(input())
+
+soma = num1+num2+num3
+
+print(soma)
